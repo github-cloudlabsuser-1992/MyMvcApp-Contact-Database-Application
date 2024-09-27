061eafde040fbe5128c5539b31c547b09bb0d0a2
Jason <Jason.liu@derko.com>
2024-09-27 00:59:23 +0000

    Azure ARM template

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,25 +12,40 @@ public class UserController : Controller
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
+
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
         }
 
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            //Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
         }
 
         // GET: User/Edit/5
@@ -38,6 +53,12 @@ public class UserController : Controller
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // POST: User/Edit/5
@@ -49,12 +70,38 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // POST: User/Delete/5
@@ -62,5 +109,21 @@ public class UserController : Controller
         public ActionResult Delete(int id, IFormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // add a search feature to the application
+        public ActionResult Search(string query)
+        {
+            // Implement the Search method here
+            var searchResults = userlist.Where(u => u.Name.Contains(query, StringComparison.OrdinalIgnoreCase)).ToList();
+            return View(searchResults);
         }
 }
